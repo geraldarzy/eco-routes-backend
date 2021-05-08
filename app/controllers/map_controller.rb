faecf9e07324848de5343baf2510885936592668
geraldarzy <geraldarzola@gmail.com>
2021-05-07 22:39:39 -0400

    setup API response to be rendered properly as JSON

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,7 +1,10 @@
 class MapController < ApplicationController
     before_action :generate_url
     def callAPI
+        # use Excon gem to call API
         response = Excon.get(@url)
+
+        #substitute inorder to make response a proper json obj
         render json: response.body.gsub('=>', ':')
     end
 
@@ -18,11 +21,4 @@ class MapController < ApplicationController
         @url = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start_long + ',' + start_lat + ';' + end_long + ',' + end_lat + '?geometries=geojson&access_token=' + access_token
     end
 end
-# http://localhost:3000/origin-destination/:start_long/:start_lat/:end_long/:end_lat
-# http://localhost:3000/origin-destination/-73;8891016/40;7410592/-73;9896416/40;7410592
-# http://localhost:3000/origin-destination/73.11/startlat/1/2
-# in mapbox, long first, lat second
-# var end = [-73.9896416, 40.7410592];
-# var start = [-73.8891016, 40.7410592];
-# var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
 

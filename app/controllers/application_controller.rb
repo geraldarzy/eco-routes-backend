e743bcad1abbd44ade731e0326df6de418f704e1
geraldarzy <geraldarzola@gmail.com>
2021-05-19 19:57:10 -0400

    add JWT encode&decode methods in our Application controller because all other controllers inherit from this controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+        JWT.encode(payload,'s3cr3t')
+    end
+
+    def decoded_token(token)
+        JWT.decode(token,'s3cr3t')
+    end
+
 end

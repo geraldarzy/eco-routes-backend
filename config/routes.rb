2e831f5c8dc99b52782b2179f9380170f958069d
geraldarzy <geraldarzola@gmail.com>
2021-05-19 21:03:28 -0400

    generate scaffold for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/origin-destination/:start_long/:start_lat/:end_long/:end_lat" => "map#callAPI"
 end

eab3db6d1981520591065c4fdc40134daaa7e4f0
geraldarzy <geraldarzola@gmail.com>
2021-05-20 00:33:40 -0400

    add the proper routes for user to signup/login/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users 
+  post '/login', to: 'users#login'
+  post '/signup', to: 'users#signup'
+  get '/me', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/origin-destination/:start_long/:start_lat/:end_long/:end_lat" => "map#callAPI"
 end

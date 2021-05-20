c41746435c6c8575fb5d87b4a32804de474b43dd
geraldarzy <geraldarzola@gmail.com>
2021-05-20 00:35:26 -0400

    make login action find user and return token else return error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate, only: [:show]
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && user.authenticate(params[:password])
+      # if they gave the right info, return that user object
+      token = JWT.encode({ user_id: @user.id }, 'mysecret', 'HS256')
+      render json: { user: UserSerializer.new(@user), token: token }
+    else
+      # otherwise, return some error message
+      render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+    end
+  end
+  
+  
 end

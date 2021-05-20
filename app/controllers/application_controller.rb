cd7a590593167e01d7f8a532110c327cd622fe17
geraldarzy <geraldarzola@gmail.com>
2021-05-19 20:05:43 -0400

    add begin/rescue in application controller incase of invalid token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,21 @@ class ApplicationController < ActionController::API
         JWT.encode(payload,'s3cr3t')
     end
 
-    def decoded_token(token)
-        JWT.decode(token,'s3cr3t')
+    def auth_header
+        # this is because we are expecting the header of the request to contain our beloved token
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            #this is because were expecting the token to be in {'Authorization':'Bearer <token>'} so need to spllit then get the second instance
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 's3cr3t', true, algorithm:'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
     end
 
 end

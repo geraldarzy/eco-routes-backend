0893bc032a97bb23c2d8c975e7ff35b669dda7a3
geraldarzy <geraldarzola@gmail.com>
2021-05-21 18:48:38 -0400

    if there are trips in trip book, render trips, else render message

--- app/controllers/trip_books_controller.rb
+++ app/controllers/trip_books_controller.rb
@@ -3,5 +3,10 @@ class TripBooksController < ApplicationController
     def show
         tripbook = TripBook.find_by(id:params['id'])
         #return stuff
+        if tripbook.trips.count > 0
+            render json: { "tripbook_#{params['id']}_trips": tripbook.trips }
+        else
+            render json: {message: 'No trips available'}
+        end
     end
 end
